83ced2180853bbf06015948c195f59be5b3b000b
HuuBng <holehuubang2005@gmail.com>
2024-01-17 21:24:27 +0700

    Refactor

--- Workshop 1/ws1-pt2-004.c
+++ Workshop 1/ws1-pt2-004.c
@@ -4,8 +4,10 @@ int main() {
   int num1, num2, temp;
 
   do {
-    printf("Enter num1: "); scanf("%d", &num1);
-    printf("Enter num2: "); scanf("%d", &num2);
+    printf("Enter num1: ");
+    scanf("%d", &num1);
+    printf("Enter num2: ");
+    scanf("%d", &num2);
 
     temp = num1;
     num1 = num2;
@@ -13,7 +15,7 @@ int main() {
 
     printf("Number 1: %d\n", num1);
     printf("Number 2: %d\n", num2);
-  } while(num1 != 0 && num2 != 0);
+  } while (num1 != 0 && num2 != 0);
 
   return 0;
 }
