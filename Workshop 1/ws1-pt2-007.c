bff8b7af7fc00383abe4acc1ef1801837fc956bf
HuuBng <holehuubang2005@gmail.com>
2024-01-17 08:43:59 +0700

    Refactor

--- Workshop 1/ws1-pt2-007.c
+++ Workshop 1/ws1-pt2-007.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 
-int main()
-{
-	char c1 = 0, c2 = 0, t, c; int d;
+int main() {
+  char c1 = 0, c2 = 0, t, c;
+  int d;
 
-	printf("Enter c1: "); scanf(" %c", &c1);
-	printf("Enter c2: "); scanf(" %c", &c2);
+  printf("Enter c1: ");
+  scanf(" %c", &c1);
+  printf("Enter c2: ");
+  scanf(" %c", &c2);
 
-	if(c1 > c2)
-	{
-		t = c1;
-		c1 = c2;
-		c2 = t;
-	}
-	d = c2 - c1;
+  if (c1 > c2) {
+    t = c1;
+    c1 = c2;
+    c2 = t;
+  }
+  d = c2 - c1;
 
-	printf("Difference: %d\n", d);
+  printf("Difference: %d\n", d);
 
-	for(c = c1; c <= c2; c++)
-		printf("%c:\t%d,\t%o,\t%X\n", c, c, c, c);
+  for (c = c1; c <= c2; c++)
+    printf("%c:\t%d,\t%o,\t%X\n", c, c, c, c);
 
-	return 0;
+  return 0;
 }
