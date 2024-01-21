3b165b3096cf900c3404850186838935e538a1e6
HuuBng <holehuubang2005@gmail.com>
2024-01-21 21:13:24 +0700

    Add ws2-pt1-007

---
+++ Workshop 2/ws2-pt1-007.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int sumDigits(int n);
+
+int main() {
+  int n;
+
+  do {
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    if (n >= 0)
+      printf(" total %d\n", sumDigits(n));
+  } while (n >= 0);
+
+  return 0;
+}
+
+int sumDigits(int n) {
+  int sum = 0;
+  do {
+    int remainder = n % 10;
+    n /= 10;
+    sum += remainder;
+  } while (n > 0);
+  return sum;
+}
