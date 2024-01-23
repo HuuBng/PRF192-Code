233714d97d79fe97a1cc0063d588d6994d7c0abd
HuuBng <holehuubang2005@gmail.com>
2024-01-23 20:26:28 +0700

    Add ws2-pt2-008

---
+++ Workshop 2/Part 2/ws2-pt2-008.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int isPrime(int n);
+
+int main() {
+  int n;
+  do {
+    printf("Enter n: ");
+    scanf("%d", &n);
+  } while (n < 2);
+
+  isPrime(n) ? printf("%d is prime", n) : printf("%d is not prime", n);
+
+  return 0;
+}
+
+int isPrime(int n) {
+  int i;
+  for (i = 2; i < n; i++) {
+    if (n % i == 0)
+      return 0;
+  }
+  return 1;
+}
