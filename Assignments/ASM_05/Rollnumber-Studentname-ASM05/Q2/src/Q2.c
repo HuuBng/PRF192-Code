bfadae92438fa5d214807c448d5aaef75a5a6861
HuuBng <holehuubang2005@gmail.com>
2024-01-30 22:20:13 +0700

    Add ASM05 placeholder

---
+++ Assignments/ASM_05/Rollnumber-Studentname-ASM05/Q2/src/Q2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+//---------------------------------------
+double sum(int n){	
+	//Write your statements here
+    //
+	//End your statements   
+}
+/*
+========================DO NOT EDIT GIVEN STATEMENTS IN THE MAIN FUNCTION.============================
+===IF YOU CHANGE, THE GRADING SOFTWARE CAN NOT FIND THE OUTPUT RESULT TO SCORE, THUS THE MARK IS 0.===
+*/
+int main()
+{ 
+  system("cls");
+  printf("\nTEST Q2 (2 marks):\n");
+  int n;
+  double result;
+  printf("Enter n = "); scanf("%d",&n);   
+  result = sum(n);
+  printf("\nOUTPUT:\n");
+  printf("%.2lf",result);
+  printf("\n");
+  system ("pause");
+  return(0);  
+}
+//===============================================================================
