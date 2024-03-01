9c589e2e0467d18fde8c3c3d840da10b0d482a49
HuuBng <holehuubang2005@gmail.com>
2024-03-01 14:50:50 +0700

    Upgrade to v3

--- Other/MAE101 - 010.c
+++ Other/MAE101 - 010.c
@@ -1,13 +1,13 @@
-// Version: 2
+// Version: 3
 
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX 101
+#define MAX 1000
 
-void inputMatrix(float mat[][MAX], int row, int column);
+void inputMatrix(float **mat, int row, int column);
 void inputRowColumn(char id, int *row, int *column);
-void printMatrix(float mat[][MAX], int row, int column);
+void printMatrix(float **mat, int row, int column);
 void menu() {
   printf("\n");
   printf("================================\n");
@@ -20,7 +20,7 @@ void menu() {
 }
 
 int main() {
-  int choice;
+  int choice, ii;
   do {
     menu();
     printf("\nEnter choice: ");
@@ -28,15 +28,21 @@ int main() {
     switch (choice) {
     case 1: { // m.A * m.B
 
-      // Create matrix A
-      float matA[MAX][MAX];
+      // Create matrix A (heap)
       int rowA, columnA;
       inputRowColumn('A', &rowA, &columnA);
+      float **matA = (float **)malloc(rowA * sizeof(float *));
+      for (ii = 0; ii < rowA; ii++) {
+        matA[ii] = (float *)malloc(columnA * sizeof(float));
+      }
 
-      // Create matrix B
-      float matB[MAX][MAX];
+      // Create matrix B (heap)
       int rowB, columnB;
       inputRowColumn('B', &rowB, &columnB);
+      float **matB = (float **)malloc(rowB * sizeof(float *));
+      for (ii = 0; ii < rowB; ii++) {
+        matB[ii] = (float *)malloc(columnB * sizeof(float));
+      }
 
       // Check compatible
       if (columnA != rowB) {
@@ -46,14 +52,18 @@ int main() {
       }
 
       // Input matrix
-      printf("Enter matrix A:\n");
+      printf("\nEnter matrix A:\n");
       inputMatrix(matA, rowA, columnA);
       printf("Enter matrix B:\n");
       inputMatrix(matB, rowB, columnB);
 
-      // Create a matrix C to store result
-      float matC[MAX][MAX];
+      // Create a matrix C (heap) to store result
       int rowC = rowA, columnC = columnB;
+      float **matC = (float **)malloc(rowC * sizeof(float *));
+      for (ii = 0; ii < rowC; ii++) {
+        matC[ii] = (float *)malloc(columnC * sizeof(float));
+      }
+
       int i, j, k;
       for (i = 0; i < rowA; i++) {
         for (j = 0; j < columnB; j++) {
@@ -76,15 +86,21 @@ int main() {
     }
     case 2: { // m.A + m.B, m.A - m.B
 
-      // Create matrix A
-      float matA[MAX][MAX];
+      // Create matrix A (heap)
       int rowA, columnA;
       inputRowColumn('A', &rowA, &columnA);
+      float **matA = (float **)malloc(rowA * sizeof(float *));
+      for (ii = 0; ii < rowA; ii++) {
+        matA[ii] = (float *)malloc(columnA * sizeof(float));
+      }
 
-      // Create matrix B
-      float matB[MAX][MAX];
+      // Create matrix B (heap)
       int rowB, columnB;
       inputRowColumn('B', &rowB, &columnB);
+      float **matB = (float **)malloc(rowB * sizeof(float *));
+      for (ii = 0; ii < rowB; ii++) {
+        matB[ii] = (float *)malloc(columnB * sizeof(float));
+      }
 
       // Check compatible
       if (rowA != rowB || columnA != columnB) {
@@ -94,19 +110,30 @@ int main() {
       }
 
       // Input matrix
-      printf("Enter matrix A:\n");
+      printf("\nEnter matrix A:\n");
       inputMatrix(matA, rowA, columnA);
       printf("Enter matrix B:\n");
       inputMatrix(matB, rowB, columnB);
 
-      // Create matrix C(+) & D(-) to store result
-      float matC[MAX][MAX], matD[MAX][MAX];
+      // Create a matrix C (+)(heap) to store result
+      int rowC = rowA, columnC = columnA;
+      float **matC = (float **)malloc(rowC * sizeof(float *));
+      for (ii = 0; ii < rowC; ii++) {
+        matC[ii] = (float *)malloc(columnC * sizeof(float));
+      }
+
+      // Create a matrix D (-)(heap) to store result
+      int rowD = rowA, columnD = columnA;
+      float **matD = (float **)malloc(rowD * sizeof(float *));
+      for (ii = 0; ii < rowD; ii++) {
+        matD[ii] = (float *)malloc(columnD * sizeof(float));
+      }
+
       int i, j;
       for (i = 0; i < rowA; i++) {
-        for (j = 0; j < columnA; j++) {
-          matC[i][j] = matD[i][j] = 0;
-          matC[i][j] += matA[i][j] + matB[i][j];
-          matD[i][j] += matA[i][j] - matB[i][j];
+        for (j = 0; j < columnB; j++) {
+          matC[i][j] = matA[i][j] + matB[i][j];
+          matD[i][j] = matA[i][j] - matB[i][j];
         }
       }
 
@@ -124,10 +151,13 @@ int main() {
     }
     case 3: { // m.A * real number
 
-      // Input matrix A
-      float matA[MAX][MAX];
+      // Create & Input matrix A (heap)
       int rowA, columnA;
       inputRowColumn('A', &rowA, &columnA);
+      float **matA = (float **)malloc(rowA * sizeof(float *));
+      for (ii = 0; ii < rowA; ii++) {
+        matA[ii] = (float *)malloc(columnA * sizeof(float));
+      }
       inputMatrix(matA, rowA, columnA);
 
       // Input real number
@@ -135,11 +165,16 @@ int main() {
       printf("Enter a real number: ");
       scanf("%f", &number);
 
-      // Create a matrix C to store result
-      float matC[MAX][MAX];
+      // Create a matrix C (heap) to store result
+      int rowC = rowA, columnC = columnA;
+      float **matC = (float **)malloc(rowC * sizeof(float *));
+      for (ii = 0; ii < rowC; ii++) {
+        matC[ii] = (float *)malloc(columnC * sizeof(float));
+      }
+
       int i, j;
-      for (i = 0; i < rowA; i++) {
-        for (j = 0; j < columnA; j++) {
+      for (i = 0; i < rowC; i++) {
+        for (j = 0; j < columnC; j++) {
           matC[i][j] = matA[i][j] * number;
         }
       }
@@ -154,15 +189,21 @@ int main() {
     }
     case 4: { // m.A - m.B + real number
 
-      // Create matrix A
-      float matA[MAX][MAX];
+      // Create matrix A (heap)
       int rowA, columnA;
       inputRowColumn('A', &rowA, &columnA);
+      float **matA = (float **)malloc(rowA * sizeof(float *));
+      for (ii = 0; ii < rowA; ii++) {
+        matA[ii] = (float *)malloc(columnA * sizeof(float));
+      }
 
-      // Create matrix B
-      float matB[MAX][MAX];
+      // Create matrix B (heap)
       int rowB, columnB;
       inputRowColumn('B', &rowB, &columnB);
+      float **matB = (float **)malloc(rowB * sizeof(float *));
+      for (ii = 0; ii < rowB; ii++) {
+        matB[ii] = (float *)malloc(columnB * sizeof(float));
+      }
 
       // Check compatible
       if (rowA != rowB || columnA != columnB) {
@@ -172,7 +213,7 @@ int main() {
       }
 
       // Input matrix
-      printf("Enter matrix A:\n");
+      printf("\nEnter matrix A:\n");
       inputMatrix(matA, rowA, columnA);
       printf("Enter matrix B:\n");
       inputMatrix(matB, rowB, columnB);
@@ -182,11 +223,16 @@ int main() {
       printf("Enter a real number: ");
       scanf("%f", &number);
 
-      // Create a matrix C to store result
-      float matC[MAX][MAX];
+      // Create a matrix C (heap) to store result
+      int rowC = rowA, columnC = columnA;
+      float **matC = (float **)malloc(rowC * sizeof(float *));
+      for (ii = 0; ii < rowC; ii++) {
+        matC[ii] = (float *)malloc(columnC * sizeof(float));
+      }
+
       int i, j;
-      for (i = 0; i < rowA; i++) {
-        for (j = 0; j < columnA; j++) {
+      for (i = 0; i < rowC; i++) {
+        for (j = 0; j < columnC; j++) {
           matC[i][j] = matA[i][j] - matB[i][j] + number;
         }
       }
@@ -210,7 +256,7 @@ int main() {
   return 0;
 }
 
-void inputMatrix(float mat[][MAX], int row, int column) {
+void inputMatrix(float **mat, int row, int column) {
   int i, j;
   for (i = 0; i < row; i++) {
     for (j = 0; j < column; j++) {
@@ -226,10 +272,10 @@ void inputRowColumn(char id, int *row, int *column) {
     scanf("%d", row);
     printf("Enter number of column for matrix %c: ", id);
     scanf("%d", column);
-  } while (*row < 0 || *column < 0);
+  } while (*row < 0 || *column < 0 || *row > MAX || *column > MAX);
 }
 
-void printMatrix(float mat[][MAX], int row, int column) {
+void printMatrix(float **mat, int row, int column) {
   int i, j;
   for (i = 0; i < row; i++) {
     for (j = 0; j < column; j++) {
